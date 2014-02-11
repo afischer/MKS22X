e05368bf07560ef280f9cc539f21c32a9a9ad62e
Andrew Fischer <afischer15@mac.com>
2014-02-11 12:14:58 -0500

    fibItr no longer uses for loop

--- 2012-02-10/Recursion.java
+++ 2012-02-10/Recursion.java
@@ -14,42 +14,46 @@ public class Recursion {
 
     public static int fib(int n){
 	if (n == 0){
-	    return n;
+	    return 0;
 	}
 	if (n == 1 ){
 	    return 1;
 	}
-	else {
-	    return fib(n-1) + fib(n-2);
-	}
-	    
+	return fib(n-1) + fib(n-2);
     }
 
-    public static int fibIter(int n) {
-	int twoago = 0;
-	int prev = 1;
-	for(int i=0; i<n; i++) {
-	    int temp = twoago;
-	    twoago = prev;
-	    prev = temp + prev;
+    public static int fibItr(int n) {
+	if (n == 0){
+	    return 0;
 	}
-	return prev;
+	if (n == 1 ){
+	    return 1;
+	}
+	return fibHelper(0,1,n-2);
     }
         
+    public static int fibHelper(int a, int b, int n){
+	if(n==0){
+	    return a+b;
+	}
+	else{
+	    return fibHelper(b, a+b, n-1);
+	}
+    }
 
     public static void main (String[] args){
 	System.out.println(fact(5));
 	System.out.println(fact(12));
 
 	System.out.println(fib(1));
-	System.out.println(fib(5));
-	System.out.println(fib(12));
+	System.out.println(fib(3));
+	System.out.println(fib(4));
 	System.out.println(fib(25));
 
-	System.out.println(fibIter(1));
-	System.out.println(fibIter(5));
-	System.out.println(fibIter(12));
-	System.out.println(fibIter(25));
+	System.out.println(fibItr(1));
+	System.out.println(fibItr(3));
+	System.out.println(fibItr(4));
+	System.out.println(fibItr(25));
 
     }
 }
